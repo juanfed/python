f6a12bcd6623df4acd0c9e7b4fca094fe3850fdb
juanfed <juanfed12345@gmail.com>
2022-05-26 19:47:48 -0500

    ejercicios

---
+++ Aprendiendo Python/025.py
@@ -0,0 +1,9 @@
+# Funcioens en python
+
+# funcion que me sume los numeros que le estoy pasando
+
+def sumarNumeros(n1, n2):
+    suma = n1 + n2
+    return print(f"el resultado de la suma fue de: {suma}")
+
+sumarNumeros(5, 7)
---
+++ Aprendiendo Python/030.py
@@ -0,0 +1,5 @@
+# tuplas y conjuntos
+# list = [1,2,3,4,5,7,7,7,6]
+
+# conjunto1 = set(list)
+# print(conjunto1)
